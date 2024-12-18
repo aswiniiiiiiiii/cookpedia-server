2af3a31e1120dcd3a5045aaebcfc06caf8bacfbb
aswiniiiiiiiii <aswinips787@gmail.com>
2024-12-18 11:35:00 +0530

    cookpedia-server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const router = require('./routes/router')
-require('../cookpedia-server/config/connection')
+require('./config/connection')
 
 const cookpediaServer = express()
 
